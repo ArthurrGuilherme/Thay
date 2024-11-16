f574ab94f53342d2f58280e1ba0b5f539ba8ab7d
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-11-16 16:14:28 -0300

    Finalização do pagina principal

---
+++ js/revel.js
@@ -0,0 +1,12 @@
+ScrollReveal({ reset: true });
+
+    ScrollReveal().reveal('#Arrow', 
+        {    
+            duration: 6500, reset: true, distance: '0px' , origin: 'top',
+        }
+    );
+    ScrollReveal().reveal('#Legend', 
+        {    
+            duration: 1500, reset: true, distance: '50px' , origin: 'left',
+        }
+    );
